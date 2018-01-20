a24f91de171aca6fb7cf917022fb1430243481f8
PachamattlaNagaSwathi <35615398+PachamattlaNagaSwathi@users.noreply.github.com>
2018-01-20 14:10:33 -0600

    making up mind

--- automation.java
+++ automation.java
@@ -1,3 +1,6 @@
 no main feild reqiured when working here
 my baby branch updated changes will occur here
 cant find in master 
+need to make up myself very strong 
+being alone is harder but get used to it
+u will be ok soon
